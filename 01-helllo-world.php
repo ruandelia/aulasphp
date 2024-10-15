0f5bf5763502d6bf55538ee896034c1604b0a283
ruandelia <ruandelia2@gmail.com>
2024-10-15 16:28:14 -0300

    comando echo

---
+++ 01-helllo-world.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+    
+    <!-- Delimitador de abertura-->
+    <?php
+    echo 'olá, mundo';
+    echo '<p style = "color:red">Programador Web - Senac Penha </p>' ;
+    ?>
+    <!-- delimitador de fechamento-->
+
+
+    <script>
+        console.log(" 'js' ")
+    </script>
+</body>
+</html>
