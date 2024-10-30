d6ab19fe2b82e62ffd19653b6f7e48fb45865f4b
ruandelia <ruandelia2@gmail.com>
2024-10-30 14:01:20 -0300

    teste de php

---
+++ testes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes PHP</title>
+</head>
+<body>
+    
+    <h1 class=""></h1>
+    
+    <h2 class="">s</h2>
+</body>
+</html>
